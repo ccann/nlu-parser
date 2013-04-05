7f5462bfb48b98810cb0e73bdffa0f2b02f60d85
Cody Canning <cocanning11@gmail.com>
2013-04-05 15:00:13 -0400

    nondeterministic parser seems to be working

--- src/nlu_parser/core.clj
+++ src/nlu_parser/core.clj
@@ -1,3 +1,12 @@
+;; @author  ccann
+;; CS 150NLD: Situated Natural Language Understanding on Robots
+;;
+;; NOTE: type-raising is assumed to be in the lexicon, and only on atomic entries.
+;;
+;;  TODO BFS search through stack-space
+;; TODO expand lexicon to a specific domain
+
+
 (ns nlu-parser.core)
 
 (defrecord Atom [cat])
@@ -14,13 +23,6 @@
 ;;                      "/"
 ;;                      (Atom. 'NP))
 ;;           (fn [x] (fn [y] ['loves y x]))])
-
-;; (def steve [(Atom. 'NP)
-;;             'steve])
-
-;; "andie" (Functor. (Atom. 'S)
-;;                                 "/"
-;;                                 (Functor. (Atom. 'S) "\\" (Atom. 'NP)))
 
 ;; Returns the result of applying the forward type-raising combinator to atom
 (defn forward-tr [atom]
@@ -49,9 +51,9 @@
         (:cat t)))
 
 ;; compose Types t1 and t2
-(defn compose [t1 t2]
+(defn combine [t1 t2]
   (do
-    (println "COMPOSING" (type-to-string t1) "and" (type-to-string t2)) 
+    (println "COMBINING" (type-to-string t1) "and" (type-to-string t2)) 
     (cond (and (is-functor? t1) (is-functor? t2))
           (if (and (= (:dir t2) "/") (= (:dir t1) "/"))
             (Functor. (:ret t1) "/" (:arg t2))
@@ -64,7 +66,7 @@
            (= (Atom. 'T) (:ret (:arg t1))))
     (if (is-functor? (:ret t2))
       (= (:arg (:ret t2)) (:arg (:arg t1)))
-      (= (:arg t2) (:arg (:arg t1))))
+      (and (= (:arg t2) (:arg (:arg t1))) (= (:dir (:arg t1)) (:dir t2))))
     false))
 
 ;; returns the unification of t1 with t2 (replaces 'T with the return value of t2)
@@ -79,10 +81,10 @@
   (cond (and (is-functor? t1) (is-functor? t2))
         (cond
          ;; > Forward composition combinator
-         (and (= (:dir t2) "/") (= (:dir t1) "/"))
+         (and (= (:dir t1) "/") (= (:dir t2) "/"))
           (= (:arg t1) (:ret t2))
          ;; < Backward composition combinator
-         (and (= (:dir t2) "\\") (= (:dir t1) "\\"))
+         (and (= (:dir t1) "\\") (= (:dir t2) "\\"))
          (= (:ret t1) (:arg t2)))
         :else false))
 
@@ -90,10 +92,10 @@
 (defn app-combinable? [t1 t2]
   (cond 
    ;; > Forward application combinator
-   (is-functor? t1)
+   (and (is-functor? t1) (is-atom? t2))
    (and (= (:dir t1) "/") (= (:arg t1) t2))
    ;; < Backward application combinator
-   (is-functor? t2)
+   (and (is-atom? t1) (is-functor? t2))
    (and (= (:dir t2) "\\") (= (:arg t2) t1))
    :else false))
  
@@ -105,7 +107,6 @@
 ;;     [(compose (first t2)(first t1))
 ;;      ((second t2) (second t1))])))
 
-;;(lexicon (first sentence))
 
 ;; returns the collection of lexical entries for the word, including type-raised atoms
 (defn get-lexical-entries [word]
@@ -138,40 +139,52 @@
             (cons entry stack)))
       stack)))
 
-;; non-deterministic parse 
+;; returns true if t1 and t2 are reducible
+(defn reducible? [t1 t2]
+  (or (comp-combinable? t1 t2)
+      (app-combinable? t1 t2)
+      (if (and (is-functor? t1) (is-functor? t2))
+        (if (and (and (= (:dir t2) "/") (= (:dir t1) "/"))
+                 (unifiable? t1 t2))
+          (comp-combinable? (unify t1 t2) t2)
+          false)
+        (if (and (and (= (:dir t2) "\\") (= (:dir t1) "\\"))
+                 (unifiable? t2 t1))
+          (comp-combinable? t1 (unify t2 t1))
+          false))))
+
+;; returns the reduction of t1 and t2 -- call reducible? first as a check
+(defn reduce-stack [t1 t2]
+  (if (or (comp-combinable? t1 t2) (app-combinable? t1 t2))
+    (combine t1 t2)
+    (if (and (= (:dir t2) "/") (= (:dir t1) "/"))
+      (combine (unify t1 t2) t2)
+      (combine t1 (unify t2 t1)))))
+
+;; returns true if the sentence has a valid parse from random lexical selections
 (defn non-det-parse [sentence stack]
-  (do (println "\nSTACK:" (reverse (map type-to-string stack)))
-      (println "INPUT:" sentence)
-      (let [s (shift sentence stack)
-            sent (rest sentence)
-            t1 (first s)
-            t2 (second s)]
-        (do 
-          ;; sentence empty, all words have been pushed to stack
-          (if (empty? sent)
-            (cond (= (count s) 1)
-                  (println "found valid parse:" (:cat (first s)))
-                  
-                  (or (comp-combinable? t2 t1) (app-combinable? t2 t1))
-                  (non-det-parse sent (cons (compose t2 t1) (rest (rest s))))
-                  
-                  (and (and (= (:dir t2) "/") (= (:dir t1) "/"))
-                       (unifiable? t2 t1))
-                  (non-det-parse sent (cons (compose (unify t2 t1) t1) (rest (rest s))))
-                  
-                  (and (and (= (:dir t2) "\\") (= (:dir t1) "\\"))
-                       (unifiable? t1 t2))
-                  (non-det-parse sent (cons (compose t2 (unify t1 t2)) (rest (rest s))))
-                  
-                  :else (do (println "failed to find a valid parse")
-                            s))
-            
-            ;; sentence non-empty
-            (if (> (count s) 1)
-              (if (composable? t2 t1)
-                (non-det-parse sent (cons (compose t2 t1) (rest (rest s))))
-                (non-det-parse sent s))
-              (non-det-parse sent s)))))))
+  (let [t1 (second stack)
+        t2 (first stack)]
+    (do (println "\nSTACK:" (reverse (map type-to-string stack)))
+        (println "INPUT:" sentence)
+        (if (not (empty? sentence))
+          (if (< (count stack) 2)
+            ;; recurse on shifted stack and rest of sentence
+            (non-det-parse (rest sentence) (shift sentence stack))
+            ;; else if reducible, recurse on reduced stack
+            (if (reducible? t1 t2)
+              (non-det-parse sentence (cons (reduce-stack t1 t2) (rest (rest stack))))
+              ;; if not reducible, recurse on shifted stack and rest of sentence
+              (non-det-parse (rest sentence) (shift sentence stack))))
+          (if (< (count stack) 2)
+            (do (println "found valid non-det-parse:" (type-to-string t2))
+                true)
+            (if (reducible? t1 t2)
+              ;; if reducible, recurse on reduced stack
+              (non-det-parse sentence (cons (reduce-stack t1 t2) (rest (rest stack))))
+              ;; otherwise return failure
+              (do (println "failed to find a valid non-det-parse")
+                  false)))))))
 
 
 (println "\n---------------------------")
@@ -180,15 +193,7 @@
 (def example1 '("the" "dog" "bit" "John"))
 (def example2 '("andie" "saw" "the" "dog"))
 
-(non-det-parse example2 '())
-
-;; DONE type raising, Unification?
-;; DONE multiple entries for lexical items, choosing mechanism (must demonstrate ambiguity)
-;; TODO BFS search through stack-space
-;; TODO expand lexicon to a specific domain
+(non-det-parse example3 '())
 
 
-;; The parser: consults the lexicon to find out the possible lexical categories and
-;; semantic representations for the input word
 
-;; type-raising is assumed to be in the lexicon, and only on atomic entries.
