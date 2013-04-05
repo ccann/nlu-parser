afa7720bc5b5ed9998c0f9f9c2bfb79347a3b56c
Cody Canning <cocanning11@gmail.com>
2013-04-05 17:54:59 -0400

    added semantics; seems to be a problem with the application order of semantic representations after type-raising

--- src/nlu_parser/core.clj
+++ src/nlu_parser/core.clj
@@ -1,13 +1,17 @@
 ;; @author  ccann
-;; CS 150NLD: Situated Natural Language Understanding on Robots
+;; COMP150NLD: Situated Natural Language Understanding on Robots
+;; Assignment 2: Parser
+;;
 ;;
 ;; NOTE: type-raising is assumed to be in the lexicon, and only on atomic entries.
 ;;
-;;  TODO BFS search through stack-space
+;; TODO BFS search through stack-space
 ;; TODO expand lexicon to a specific domain
+;; TODO add semantics?? need higher-order representations in lexicon
 
 
 (ns nlu-parser.core)
+(use '[clojure.string :only (join split)])
 
 (defrecord Atom [cat])
 (defrecord Functor [ret dir arg])
@@ -18,11 +22,11 @@
 ;; (def andie [(Functor. (Atom. 'S)
 ;;                      "/"
 ;;                        (Functor. (Atom. 'S) "\\" (Atom. 'NP)))
-;;             (fn [x] (x 'andie))])
+;;             ])
 ;; (def see [(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP))
 ;;                      "/"
 ;;                      (Atom. 'NP))
-;;           (fn [x] (fn [y] ['loves y x]))])
+;;          ])
 
 ;; Returns the result of applying the forward type-raising combinator to atom
 (defn forward-tr [atom]
@@ -31,17 +35,42 @@
 (defn backward-tr [atom]
   (Functor. (Atom. 'T) "\\" (Functor. (Atom. 'T) "/" atom)))
 
-(def lexicon {"andie" [(Atom. 'NP)] 
-              "saw" [(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP))
-                               "/"
-                               (Atom. 'NP))
-                     (Atom. 'N)]
-              "steve" [(Atom. 'NP)]
-              "loves" [(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))]
-              "the" [(Functor. (Atom. 'NP) "/" (Atom. 'N))]
-              "dog" [(Atom. 'N)]
-              "John" [(Atom. 'NP)]
-              "bit" [(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))]})
+(def lexicon {"andie" {(Atom. 'NP) 'andie
+                       (forward-tr (Atom. 'NP)) (fn [x] (x 'andie))}
+              
+              "saw" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))
+                     (fn [y] (fn [x] ['see y x]))
+                     (Atom. 'N) (fn [x] ['saw x])}
+
+              "steve" {(Atom. 'NP) 'steve
+                       (forward-tr (Atom. 'NP)) (fn [x] (x 'steve))}
+              
+              "loves" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))
+                       (fn [y] (fn [x] ['love y x]))}
+              
+              "the" {(Functor. (Atom. 'NP) "/" (Atom. 'N))
+                     (fn [p] ['def p])}
+
+              "flowers" {(Atom. 'N) (fn [x] ['flowers x])
+                         (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
+                         (fn [q] (fn [x] [(q 'flowers) x]))}
+              
+              "sent" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'PP))
+                      (fn [y] (fn [x] ['summon y x]))}
+              
+              "for" {(Functor. (Atom. 'PP) "/" (Atom. 'NP)) (fn [x] x)}
+              
+              "patient" {(Atom. 'N) (fn [x] ['patient x])
+                         (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
+                         (fn [q] (fn [x] [(q 'patient) x]))}
+              
+              "dog" {(Atom. 'N) (fn [x] ['dog x])}
+
+              "john" {(Atom. 'NP) 'john
+                       (forward-tr (Atom. 'NP)) (fn [x] (x 'john))}
+
+              "bit" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))
+                     (fn [y] (fn [x] ['bite y x]))}})
 
 ;; returns a string representation of the complex or simple type
 (defn type-to-string [t]
@@ -51,15 +80,15 @@
         (:cat t)))
 
 ;; compose Types t1 and t2
-(defn combine [t1 t2]
+(defn combine [t1 t2 m1 m2]
   (do
     (println "COMBINING" (type-to-string t1) "and" (type-to-string t2)) 
     (cond (and (is-functor? t1) (is-functor? t2))
           (if (and (= (:dir t2) "/") (= (:dir t1) "/"))
-            (Functor. (:ret t1) "/" (:arg t2))
-            (Functor. (:ret t2) "\\" (:arg t1)))
-          (is-functor? t1) (:ret t1)
-          :else          (:ret t2))))
+            [(Functor. (:ret t1) "/" (:arg t2)) (m1 m2)]
+            [(Functor. (:ret t2) "\\" (:arg t1)) (m2 m1)])
+          (is-functor? t1) [(:ret t1) (m1 m2)]
+          :else          [(:ret t2) (m2 m1)])))
 
 (defn unifiable? [t1 t2]
   (if (and (is-functor? t2) (is-functor? t1)
@@ -71,10 +100,9 @@
 
 ;; returns the unification of t1 with t2 (replaces 'T with the return value of t2)
 (defn unify [t1 t2]
-  (do (println "UNIFYING:" (type-to-string t1) "with" (type-to-string t2))
-      (if (is-functor? (:ret t2))
+  (if (is-functor? (:ret t2))
         (assoc (assoc t1 :arg (:ret t2)) :ret (:ret (:ret t2)))
-        (assoc (assoc t1 :arg t2) :ret (:ret t2)))))
+        (assoc (assoc t1 :arg t2) :ret (:ret t2))))
 
 ;; returns true if two Types are combinable by composition combinators
 (defn comp-combinable? [t1 t2]
@@ -107,6 +135,7 @@
 ;;     [(compose (first t2)(first t1))
 ;;      ((second t2) (second t1))])))
 
+;;COMBINING ((S\NP)/NP) and (NP/N)
 
 ;; returns the collection of lexical entries for the word, including type-raised atoms
 (defn get-lexical-entries [word]
@@ -121,23 +150,31 @@
                    (rest ents))
             (recur accum (rest ents))))))))
 
+(defn get-lexical-entries-2 [word]
+  (keys (lexicon word)))
+
 ;; returns a random entry for the word in the lexicon
 (defn random-lookup [word]
-  (let [entries (get-lexical-entries word)
+  (let [entries (get-lexical-entries-2 word)
         selection (nth entries (rand-int (count entries)))]
     (do
       (println "SELECTING:" (type-to-string selection) "for" word)
       selection)))
 
 ;; SHIFT: push the first word's lexical entry onto the stack
-(defn shift [sentence stack]
+(defn shift [sentence stack sem]
   (let [s1 (first sentence)]
     (if (not (nil? s1))
       (let [entry (random-lookup s1)]
         (do (println  "PUSHING:" (type-to-string entry) "onto"
                       (reverse (map type-to-string stack)))
-            (cons entry stack)))
-      stack)))
+            [(cons entry stack) (cons ((lexicon s1) entry) sem)]))
+      [stack sem])))
+
+(defn shift-semantics [sentence sem]
+  (let [m1 (first sem)]
+    (if (not (nil? m1))
+      ())))
 
 ;; returns true if t1 and t2 are reducible
 (defn reducible? [t1 t2]
@@ -147,41 +184,53 @@
         (if (and (and (= (:dir t2) "/") (= (:dir t1) "/"))
                  (unifiable? t1 t2))
           (comp-combinable? (unify t1 t2) t2)
-          false)
-        (if (and (and (= (:dir t2) "\\") (= (:dir t1) "\\"))
+          (if (and (and (= (:dir t2) "\\") (= (:dir t1) "\\"))
                  (unifiable? t2 t1))
-          (comp-combinable? t1 (unify t2 t1))
-          false))))
+            (comp-combinable? t1 (unify t2 t1))
+            false))
+        false)))
 
 ;; returns the reduction of t1 and t2 -- call reducible? first as a check
-(defn reduce-stack [t1 t2]
+(defn reduce-stack [t1 t2 m1 m2]
   (if (or (comp-combinable? t1 t2) (app-combinable? t1 t2))
-    (combine t1 t2)
+    (combine t1 t2 m1 m2)
     (if (and (= (:dir t2) "/") (= (:dir t1) "/"))
-      (combine (unify t1 t2) t2)
-      (combine t1 (unify t2 t1)))))
+      (do (println "UNIFYING:" (type-to-string t1) "with" (type-to-string t2))
+          (combine (unify t1 t2) t2 m2 m1))
+      (do (println "UNIFYING:" (type-to-string t2) "with" (type-to-string t1))
+          (combine t1 (unify t2 t1) m1 m2)))))
 
 ;; returns true if the sentence has a valid parse from random lexical selections
-(defn non-det-parse [sentence stack]
+(defn non-det-parse [sentence stack sem]
   (let [t1 (second stack)
-        t2 (first stack)]
+        t2 (first stack)
+        m1 (second sem)
+        m2 (first sem)]
     (do (println "\nSTACK:" (reverse (map type-to-string stack)))
         (println "INPUT:" sentence)
         (if (not (empty? sentence))
           (if (< (count stack) 2)
-            ;; recurse on shifted stack and rest of sentence
-            (non-det-parse (rest sentence) (shift sentence stack))
+            (let [new-stack-sem (shift sentence stack sem)]
+              ;; recurse on shifted stack and rest of sentence
+              (non-det-parse (rest sentence) (first new-stack-sem) (second new-stack-sem)))
             ;; else if reducible, recurse on reduced stack
             (if (reducible? t1 t2)
-              (non-det-parse sentence (cons (reduce-stack t1 t2) (rest (rest stack))))
+              (let [ [t m] (reduce-stack t1 t2 m1 m2) ]
+                (non-det-parse sentence (cons t (rest (rest stack)))
+                               (cons m (rest (rest sem)))))
+
               ;; if not reducible, recurse on shifted stack and rest of sentence
-              (non-det-parse (rest sentence) (shift sentence stack))))
+              (let [new-stack-sem (shift sentence stack sem)] 
+                (non-det-parse (rest sentence) (first new-stack-sem) (second new-stack-sem)))))
           (if (< (count stack) 2)
             (do (println "found valid non-det-parse:" (type-to-string t2))
+                (println sem)
                 true)
             (if (reducible? t1 t2)
               ;; if reducible, recurse on reduced stack
-              (non-det-parse sentence (cons (reduce-stack t1 t2) (rest (rest stack))))
+              (let [ [t m] (reduce-stack t1 t2 m1 m2)]
+                (non-det-parse sentence (cons t (rest (rest stack)))
+                               (cons m (rest (rest sem)))))
               ;; otherwise return failure
               (do (println "failed to find a valid non-det-parse")
                   false)))))))
@@ -189,11 +238,12 @@
 
 (println "\n---------------------------")
 
-(def example3 '("andie" "loves" "steve"))
-(def example1 '("the" "dog" "bit" "John"))
-(def example2 '("andie" "saw" "the" "dog"))
+(def example3 "andie loves steve")
+(def example1 "the dog bit john")
+(def example2 "andie saw the dog")
+(def example4 "the flowers sent for the patient")
 
-(non-det-parse example3 '())
+(non-det-parse (split example3 #"\s") '() '())
 
 
 
