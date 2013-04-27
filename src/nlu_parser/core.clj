c7e6f7908bd322047e24202699d7cac2c3c4bce1
Cody Canning <cocanning11@gmail.com>
2013-04-26 20:02:30 -0400

    added situatedness, fixed lexicon, tested

--- src/nlu_parser/core.clj
+++ src/nlu_parser/core.clj
@@ -2,10 +2,10 @@
 ;; COMP150NLD: Situated Natural Language Understanding on Robots
 ;; Assignment 2: Parser
 ;;
-;;
 ;; NOTE: type-raising is assumed to be in the lexicon, and only on atomic entries.
 ;;
-;; TODO get semantics working completely (it doesn't work in the MS dissertation, so...)
+;; TODO semantics
+;; TODO add ruthless reduce first heuristic
 
 (ns nlu-parser.core)
 (use '[clojure.string :only (join split)])
@@ -42,52 +42,41 @@
                      "jagr" {(Atom. 'NP) 'jagr
                              (forward-tr (Atom. 'NP)) (fn [x] (x 'jagr))
                              (backward-tr (Atom. 'NP)) (fn [x] (x 'jagr))}
-              
-                     ;; "checked" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))
-                     ;;            (fn [y] (fn [x] ['check y x]))}
-              
-                     ;; "the" {(Functor. (Atom. 'NP) "/" (Atom. 'N))
-                     ;;        (fn [p] ['def p])}
+                           
+                     "the" {(Functor. (Atom. 'NP) "/" (Atom. 'N))
+                            (fn [p] ['def p])
+                            (Functor. (Functor. (Atom. 'S) "/"
+                                                (Functor. (Atom. 'S) "\\" (Atom. 'NP)))  "/"
+                                      (Atom. 'N))
+                            (fn [p] (fn [q] (q ['def p])))}
 
-                     ;; "fans" {(Atom. 'N) (fn [x] ['fans x])
-                     ;;         (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
-                     ;;         (fn [q] (fn [x] [(q 'fans) x]))}
+                     "fans" {(Atom. 'N) (fn [x] ['fans x])
+                             (forward-tr (Atom. 'N)) (fn [x] [])
+                             (backward-tr (Atom. 'N)) (fn [x] [])
+                             (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
+                             (fn [q] (fn [x] [(q 'fans) x]))}
               
-                     ;; "saved" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'PP))
-                     ;;          (fn [y] (fn [x] ['save y x]))}
+                     "sent" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'PP))
+                             (fn [y] (fn [x] ['save y x]))
+                             (Functor. (Functor. (Atom. 'N) "\\" (Atom. 'N)) "/" (Atom. 'PP))
+                             (fn [z] (fn [p] (fn [y] [])))}
 
-                     ;; "skated" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'PP))
-                     ;;           (fn [y] (fn [x] ['skate y x]))}
-
-                     ;; "skate" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'PP))
-                     ;;          (fn [y] (fn [x] ['skate y x]))
-                     ;;          (Atom. 'N) (fn [x] ['skate x])}
+                     "skate" {(Functor. (Atom. 'S) "\\" (Atom. 'NP))
+                              (fn [y] (fn [x] ['skate y x]))
+                              (Atom. 'N) (fn [x] ['skate x])}
                      
-                     ;; "for" {(Functor. (Atom. 'PP) "/" (Atom. 'NP)) (fn [x] x)}
+                     "for" {(Functor. (Atom. 'PP) "/" (Atom. 'NP)) (fn [x] x)}
               
-                     ;; "skater" {(Atom. 'N) (fn [x] ['skater x])
-                     ;;           (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
-                     ;;           (fn [q] (fn [x] [(q 'skater) x]))}
-
-                     ;; "player" {(Atom. 'N) (fn [x] ['player x])
-                     ;;           (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
-                     ;;           (fn [q] (fn [x] [(q 'player) x]))}
-
-                     ;; "players" {(Atom. 'N) (fn [x] ['players x])
-                     ;;            (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
-                     ;;            (fn [q] (fn [x] [(q 'players) x]))}
+                     "player" {(Atom. 'N) (fn [x] ['player x])
+                               (forward-tr (Atom. 'N)) (fn [x] [])
+                               (backward-tr (Atom. 'N)) (fn [x] [])
+                                (Functor. (Atom. 'N) "/" (Functor. (Atom. 'N) "\\" (Atom. 'N)))
+                                (fn [q] (fn [x] [(q 'player) x]))}
               
-                     ;; "puck" {(Atom. 'N) (fn [x] ['puck x])}
+                     "scored" {(Functor. (Atom. 'S) "\\" (Atom. 'NP))
+                               (fn [x] ['score x])}})         
 
-                     ;; "henrik" {(Atom. 'NP) 'henrik
-                     ;;           (forward-tr (Atom. 'NP)) (fn [x] (x 'henrik))}
-
-                     ;; "shot" {(Functor. (Functor. (Atom. 'S) "\\" (Atom. 'NP)) "/" (Atom. 'NP))
-                     ;;         (fn [y] (fn [x] ['shoot y x]))
-                     })                                      
-
-
-
+;; returns true if one type has a wild card type T that unifies with the other
 (defn unifiable? [t1 t2]
   (if (and (is-functor? t2) (is-functor? t1)
            (= (Atom. 'T) (:ret (:arg t1))))
@@ -128,7 +117,7 @@
 ;; compose Types t1 and t2
 (defn combine [t1 t2]
   (do
-    (println "COMBINING" (tts t1) "and" (tts t2)) 
+    #_(println "COMBINING" (tts t1) "and" (tts t2)) 
     (cond (and (is-functor? t1) (is-functor? t2))
           (if (and (= (:dir t2) "/") (= (:dir t1) "/"))
             (Functor. (:ret t1) "/" (:arg t2))
@@ -136,30 +125,35 @@
           (is-functor? t1)  (:ret t1)
           :else             (:ret t2))))
 
-
 ;; returns the reduction of t1 and t2 -- call reducible? first as a check
 (defn ccg-reduce-types [t1 t2]
   (if (or (comp-combinable? t1 t2) (app-combinable? t1 t2))
     (combine t1 t2)
     (if (and (= (:dir t2) "/") (= (:dir t1) "/"))
-      (do (println "UNIFYING:" (tts t1) "with" (tts t2))
+      (do #_(println "UNIFYING:" (tts t1) "with" (tts t2))
           (combine (unify t1 t2) t2))
-      (do (println "UNIFYING:" (tts t2) "with" (tts t1))
+      (do #_(println "UNIFYING:" (tts t2) "with" (tts t1))
           (combine t1 (unify t2 t1))))))
 
-;; SHIFT: push the  onto the stack
+;; returns entry pushed onto stack
 (defn ccg-push [entry stack]
   (if (not (nil? entry))
-    (let [result (cons entry stack)]
-      (do (println "PUSHING:" (tts entry) "onto" (reverse (map tts stack)))
-          result))
-    (let [result stack]
-      (do (println "ERROR!: trying to push nil onto stack")
-          result))))
+    (do #_(println "PUSHING:" (tts entry) "onto" (reverse (map tts stack)))
+        (cons entry stack))
+    (do #_(println "ERROR!: trying to push nil onto stack")
+        stack)))
 
+;; return lexical entries for word in lex
 (defn lookup [word lex]
-  (keys (lex word)))
+  (if (contains? objects-detected word)
+    (when (contains? (set (keys (lex word))) (Atom. 'N))
+      (do
+        (println "DETECTED:" word "in environment, choosing"
+                 (tts (Atom. 'N)) "for" word)
+        [(Atom. 'N)]))
+    (keys (lex word))))
 
+;; pushes word onto all stacks in ws, returns ws
 (defn ccg-shift [word ws lex]
   (let [entries (lookup word lex)]
     (if (empty? ws)
@@ -167,6 +161,7 @@
       (apply concat (for [stack ws]
                       (for [e entries] (ccg-push e stack)))))))
 
+;; if reducible, returns reduced stack, otherwise returns stack
 (defn ccg-reduce [stack]
   (let [t2 (first stack)
         t1 (second stack)]
@@ -188,27 +183,33 @@
             false))
         false)))
 
+;; returns the number of valid left-right inc parses for this sentence
 (defn ccg-parse [sent workspace lex]
-  (if (not (empty? sent))
-    (let [word (first sent)
-          ws (ccg-shift word workspace lex)]
-      (ccg-parse (rest sent)       
-                 (for [stack ws]
-                   (ccg-reduce stack))
-                 lex))
-    (let [succs (filter (partial = [(Atom. 'S)]) workspace)]
-      (if (empty? succs)
-        (println "FAILURE: could not find a valid parse.")
-        (println "SUCCESS: found" (count succs) "valid parses amidst"
-                 (count workspace) "possible parses.")))))
+  (do (pprint-ws workspace)
+      (if (not (empty? sent))
+        (let [word (first sent)
+              ws (ccg-shift word workspace lex)]
+          (do (pprint-ws ws)
+              (ccg-parse (rest sent)       
+                         (for [stack ws]
+                           (ccg-reduce stack))
+                         lex)))
+        (let [succs (filter (partial = [(Atom. 'S)]) workspace)]
+          (if (empty? succs)
+            (println "FAILURE: could not find a valid parse.")
+            (do (println "SUCCESS: found" (count succs) "valid parses amidst"
+                         (count workspace) "possible parses.")
+                (count succs)))))))
 
-
+;; pretty print the workspace; returns nil
+(defn pprint-ws [ws]
+  (println "Workspace:" (map (partial map tts) (map reverse ws))))
 
 ;; set of objects from vision that are located in the immediate environment
-(def objects-detected #{"puck" "skate"})
+(def objects-detected #{"puck"})
 
 (println "\n---------------------------")
 
-(def example3 "jagr loved 77")
-(def answer (ccg-parse (split example3 #"\s") '() hockey-lexicon))
+(def example3 "the player sent for the fans scored")
+(ccg-parse (split example3 #"\s") '() hockey-lexicon)
 
